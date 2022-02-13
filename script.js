411ec8653a10e556f1ec693abd3701b0694ebca5
Rahad-khan <clash146098@gmail.com>
2022-02-13 17:01:15 +0600

    button functionality added

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function generateRandomNumber() {
   const randomNumber = Math.round(Math.random() * 10000);
-  const randomNumberString = randomNumber + '';
+  const randomNumberString = randomNumber + "";
   if (randomNumberString.length == 4) {
-      document.getElementById("display-number").value = randomNumber;
+    document.getElementById("display-generate-number").value = randomNumber;
   } else {
-      console.log(randomNumber)
-    generateRandomNumber()
+    generateRandomNumber();
   }
-};
-
+}
+document.getElementById("calc-buttons").addEventListener("click", function (e) {
+  const buttonVAlue = e.target.innerText;
+  const typedNumbers = document.getElementById("display-typed-number");
+  if (isNaN(buttonVAlue)) {
+      if (buttonVAlue == "C") {
+          typedNumbers.value = "";
+      } else if (buttonVAlue == "<"){
+        const typedText = typedNumbers.value;
+        var substratLastChar=typedText.slice(0, -1);
+        typedNumbers.value = substratLastChar;
+      }
+  } else {
+    const previousNumber = typedNumbers.value;
+    const newNumber = previousNumber + buttonVAlue;
+    typedNumbers.value = newNumber;
+  }
+});
