75ddf6c030a7724b20e16720597001685e897f50
Rahad-khan <clash146098@gmail.com>
2022-02-13 17:13:32 +0600

    done

--- script.js
+++ script.js
@@ -11,16 +11,31 @@ document.getElementById("calc-buttons").addEventListener("click", function (e) {
   const buttonVAlue = e.target.innerText;
   const typedNumbers = document.getElementById("display-typed-number");
   if (isNaN(buttonVAlue)) {
-      if (buttonVAlue == "C") {
-          typedNumbers.value = "";
-      } else if (buttonVAlue == "<"){
-        const typedText = typedNumbers.value;
-        var substratLastChar=typedText.slice(0, -1);
-        typedNumbers.value = substratLastChar;
-      }
+    if (buttonVAlue == "C") {
+      typedNumbers.value = "";
+    } else if (buttonVAlue == "<") {
+      const typedText = typedNumbers.value;
+      var substratLastChar = typedText.slice(0, -1);
+      typedNumbers.value = substratLastChar;
+    }
   } else {
     const previousNumber = typedNumbers.value;
     const newNumber = previousNumber + buttonVAlue;
     typedNumbers.value = newNumber;
+  }
+});
+document.getElementById("is-matching").addEventListener("click", function () {
+  const generateNumber = document.getElementById(
+    "display-generate-number"
+  ).value;
+  const displayNumber = document.getElementById("display-typed-number").value;
+  const notifySucess = document.getElementById("notify-success");
+  const notifyFail = document.getElementById("notify-fail");
+  if (generateNumber == displayNumber) {
+    notifySucess.style.display = "block";
+    notifyFail.style.display = "none";
+  } else {
+    notifySucess.style.display = "none";
+    notifyFail.style.display = "block";
   }
 });
